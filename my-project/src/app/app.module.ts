9bfec6e3b819a63f0fd4af67c7a9c58dec3dbca2
Maria Herrador <mdolores.hv@gmail.com>
2023-07-13 07:28:54 +0200

    Reverted changes. Main is now on its original state

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FirstComponentComponent } from './first-component/first-component.component';
-import { SecondComponentComponent } from './second-component/second-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FirstComponentComponent,
-    SecondComponentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
